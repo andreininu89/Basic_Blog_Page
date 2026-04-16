4ce5821d6960ed875addc9c0a096e9eb57d2020f
Andrei Ninu <andrei.ninu@gmail.com>
2026-04-16 16:10:50 +0300

    Starting to create Post and Comment class models

--- blog_clone_project/blog/models.py
+++ blog_clone_project/blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    # The important caveat: if your project uses a custom user model, neither of these is ideal. In Django, the usual best practice is:
+    #     from django.conf import settings
+    #
+    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # That keeps your model compatible with custom user setups.
+
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    # edit_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comments = models.BooleanField(default=False)
